1cdce4f50a08fe90dc4475717c08af99e61354ec
Florian Becker <fb@vxapps.com>
2021-10-21 12:08:19 +0200

    Fix for visual studio and filesystem::path to string converting

--- examples/logger/main.cpp
+++ examples/logger/main.cpp
@@ -79,7 +79,11 @@ static void work() {
 
 int main() {
 
+#ifdef _WIN32
+  std::string tmpDir = std::filesystem::temp_directory_path().c_str();
+#else
   std::string tmpDir = std::filesystem::temp_directory_path();
+#endif
   std::cout << "Create tmp file: " << tmpDir + filename << std::endl;
 
   /* configure logging, if you dont it defaults to standard out logging with colors */
